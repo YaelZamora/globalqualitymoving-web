a8e66a563e4e997ded9f4eb9bfb72e99477c362c
YaelZamora <ajedrezkasparov@gmail.com>
2026-08-27 01:43:40 +0000

    Deploying to gh-pages from @ YaelZamora/GlobalQualityMoving@0360231924f9184d0531423402041b5aa9e42a3b 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "70268404" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2149965910" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
